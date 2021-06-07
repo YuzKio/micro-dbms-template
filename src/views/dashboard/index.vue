d8f818d2854394d39e779d52a3542c910efe7a6f
unknown <407249117@qq.com>
2021-06-07 22:57:32 +0800

    change home

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -8,17 +8,26 @@
       <ul class="dashboard-todo-nav">
         <li>
           <div class="dashboard-todo-nav-card">
-            <router-link to="/">连接数据库</router-link>
+            <img src="https://element.eleme.cn/static/guide.0a8462c.png" />
+            <div>
+              <router-link to="/">连接数据库</router-link>
+            </div>
           </div>
         </li>
         <li>
           <div class="dashboard-todo-nav-card">
-            <router-link to="/">查看数据库</router-link>
+            <img src="https://element.eleme.cn/static/resource.a72b8f8.png" />
+            <div>
+              <router-link to="/">查看数据库</router-link>
+            </div>
           </div>
         </li>
         <li>
           <div class="dashboard-todo-nav-card">
-            <router-link to="/">指令编写</router-link>
+            <img src="https://element.eleme.cn/static/component.bd3411b.png" />
+            <div>
+              <router-link to="/">指令编写</router-link>
+            </div>
           </div>
         </li>
       </ul>
@@ -71,20 +80,43 @@ export default {
   }
 }
 .dashboard-todo-nav {
-  margin-left: -30px;
+  margin: 30px 0 0 -30px;
   li{
     float: left;
     width: 33.3%;
     box-sizing: border-box;
     padding: 0 38px;
     list-style: none;
+    display: table;
   }
   &-card {
     width: 100%;
     height: 300px;
     box-sizing: border-box;
-    border: 1px solid #eaeefb;
+    //border: 1px solid #eaeefb;
+    border-radius: 3px;
     text-align: center;
+    position: relative;
+    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+    //transition: all .3s ease-in-out;
+    img {
+      width: 160px;
+      height: 120px;
+      margin: 50px 0 40px;
+    }
+    div {
+      color: #fff;
+      line-height: 40px;
+      height: 45px;
+      border: 1px solid #eaeefb;
+      border-radius: 5px;
+      background: #409EFF;
+      margin: 0 35px;
+      &:hover {
+        cusor: pointer;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+      }
+    }
   }
 }
 
