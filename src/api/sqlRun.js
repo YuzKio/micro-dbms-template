369c378f9c2487fb1f81778428bbe81e5e77bc50
lisiyu1997 <you@example.com>
2021-06-13 10:20:25 +0800

    runSql

---
+++ src/api/sqlRun.js
@@ -0,0 +1,10 @@
+import request from '@/utils/request'
+
+export function runSql(params) {
+  return request({
+    // url: '/vue-admin-template/table/list',
+    url: 'http://39.105.24.214/ShowDatabases',
+    method: 'post',
+    params
+  })
+}
--- src/views/panel/index.vue
+++ src/views/panel/index.vue
@@ -1,85 +1,129 @@
 <template>
-  <div class="app-container">
+  <div class="app-container container">
     <el-form ref="form" :model="form" label-width="60px" style="display: inline-block; width: 50%">
       <el-form-item>
         <el-input
+          v-model="form.textarea"
           title="指令编写"
           type="textarea"
           :rows="19"
           placeholder="请输入sql语句"
-          v-model="form.textarea"
           style="width: 450px"
-        >
-        </el-input>
+        />
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">运行</el-button>
         <el-button @click="onCancel">重置</el-button>
       </el-form-item>
     </el-form>
-    <el-table
-      v-loading="listLoading"
-      :data="tableData"
-      element-loading-text="Loading"
-      border
-      fit
-      highlight-current-row
-      style="display: inline-block; width: 50%"
-    >
-      <el-table-column align="center" label="ID">
-        <template slot-scope="scope">
-          {{ scope.$index }}
-        </template>
-      </el-table-column>
-      <el-table-column align="center" label="数据表名称">
-        <template slot-scope="scope">
-          {{ scope.row.name }}
-        </template>
-      </el-table-column>
-      <el-table-column class-name="status-col" label="操作" align="center">
-        <template>
-          <!--          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>-->
-          <el-button
-            :type="gray"
-            size="small"
-          >
-            查看
-          </el-button>
-        </template>
-        <template>
-          <!--          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>-->
-          <el-button
-            :type="gray"
-            size="small"
-          >
-            删除
-          </el-button>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="created_at" label="上次连接时间" width="200">
-        <template slot-scope="scope">
-          <i class="el-icon-time" />
-          <span>{{ scope.row.display_time }}</span>
-        </template>
-      </el-table-column>
-    </el-table>
+    <div class = "result">
+      <div class = "showRes">
+        {{status}}
+      </div>
+      <el-table
+        v-if="showTable"
+        v-loading="listLoading"
+        :data="list.data"
+        element-loading-text="Loading"
+        border
+        fit
+        highlight-current-row
+        style="display: inline-block; max-width: 90%"
+      >
+        <el-table-column
+          v-for="item in list.attrNum"
+          :key="item"
+          fixed
+          align="center"
+          :label="item"
+          :prop="item"
+          width="100"
+        >
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
 <script>
+import { runSql } from '@/api/sqlRun'
+
 export default {
   data() {
     return {
       form: {
         textarea: ''
-      }
+        // reset: false
+      },
+      status: '更新成功',
+      list: {
+        attrNum: ['date', 'name', 'province', 'city', 'address', 'zip'],
+        data: [{
+          date: '2016-05-03',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-02',
+          name: '王小虎2',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-04',
+          name: '王小虎3',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-01',
+          name: '王小虎4',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-08',
+          name: '王小虎5',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-06',
+          name: '王小虎6',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-07',
+          name: '王小虎7',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }]
+      },
+      showTable: true,
+      listLoading: false
     }
   },
   methods: {
     onSubmit() {
+      runSql(this.form.textarea).then(response => {
+        this.status = response.data.message
+        this.list = response.data.items
+        if (this.list) { this.showTable = true }
+      })
       this.$message('submit!')
     },
     onCancel() {
+      this.form.textarea = ' '
       this.$message({
         message: 'cancel!',
         type: 'warning'
@@ -93,5 +137,23 @@ export default {
 el-form-item {
   padding-left: -20px;
 }
+
+.container{
+  display: flex;
+  justify-content: space-between;
+}
+.result{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width:100%;
+  padding:0 20px;
+  box-sizing:content-box;
+}
+.showRes{
+  padding:20px 100px;
+  background-color:lightskyblue;
+}
+
 </style>
 
